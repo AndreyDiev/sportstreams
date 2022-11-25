aa6a47033f810e112bb2f648257f3163dfee8493
AndreyDiev <110460038+AndreyDiev@users.noreply.github.com>
2022-11-25 21:39:13 +0300

    feature: добавить попап регистрации

---
+++ script.js
@@ -0,0 +1,19 @@
+const popupReg = document.querySelector('.popup-reg');
+const headerRegBtn = document.querySelector('.header__register');
+const popupRegContainer = document.querySelector('.popup-reg__container');
+
+headerRegBtn.addEventListener('click', () => {
+  popupReg.classList.add('popup_active');
+  console.log('1');
+});
+
+popupReg.addEventListener("click", function (e) {
+  const target = e.target;
+  const its_menu = target == popupRegContainer || popupRegContainer.contains(target);
+  const menu_is_active = popupReg.classList.contains("popup_active");
+
+  if (!its_menu && menu_is_active) {
+      popupReg.classList.remove('popup_active');
+      console.log('2');
+  }
+});
