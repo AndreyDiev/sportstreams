ee3f957ea565aa9c9878fab6641398f12008a883
AndreyDiev <110460038+AndreyDiev@users.noreply.github.com>
2022-11-26 18:11:22 +0300

    develop: добавить разметку главной 1600px

--- script.js
+++ script.js
@@ -17,3 +17,32 @@ popupReg.addEventListener("click", function (e) {
       console.log('2');
   }
 });
+
+const popupLoginToggleBtn = document.querySelector('.popup-reg__login');
+const popupLogin = document.querySelector('.popup-log');
+const popupLogContainer = document.querySelector('.popup-log__container');
+
+popupLoginToggleBtn.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  popupReg.classList.remove('popup_active');
+  popupLogin.classList.add('popup_active');
+})
+
+const popupRegToggleBtn = document.querySelector('.popup-log__register');
+
+popupRegToggleBtn.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  popupLogin.classList.remove('popup_active');
+  popupReg.classList.add('popup_active');
+});
+
+popupLogin.addEventListener("click", function (e) {
+  const target = e.target;
+  const its_menu = target == popupLogContainer || popupLogContainer.contains(target);
+  const menu_is_active = popupLogin.classList.contains("popup_active");
+
+  if (!its_menu && menu_is_active) {
+      popupLogin.classList.remove('popup_active');
+      console.log('2');
+  }
+});
